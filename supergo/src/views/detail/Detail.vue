02312e37f00e098c6991650908a1d8680df354aa
twotwowen <two_two_wen@163.com>
2021-09-08 11:44:43 +0800

    添加详情页产品信息，商家信息，添加滚动

--- supergo/src/views/detail/Detail.vue
+++ supergo/src/views/detail/Detail.vue
@@ -1,25 +1,40 @@
 <template>
-  <div class="detail">
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :topimg="topImg"></detail-swiper>
+  <div id="detail">
+    <detail-nav-bar class="detail-nav"></detail-nav-bar>
+    <scroll class="content" @pullingUp="loadMore" ref="scroll" :probe-type="3" :pull-up-load = "true">
+      <detail-swiper :topimg="topImg"></detail-swiper>
+    <detail-base-info :goods="goods"></detail-base-info>
+    <detail-shop-info :shop="shop"></detail-shop-info>
+    </scroll>
   </div>
 </template>
 
 <script>
 import DetailNavBar from './childComps/DetailNavBar.vue';
-import {getDetail} from  'network/detail.js'
+import {getDetail,Goods,Shop} from  'network/detail.js'
 import DetailSwiper from './childComps/DetailSwiper.vue';
+import DetailBaseInfo from './childComps/DetailBaseInfo.vue';
+import DetailShopInfo from './childComps/DetailShopInfo.vue';
+
+import Scroll from 'components/common/scroll/Scroll.vue'
+
 
 export default {
   name: 'Detail',
   components:{
     DetailNavBar,
-    DetailSwiper 
+    DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    Scroll,
+  
   },
     data() {
     return {
       iid:null,
-      topImg: []
+      topImg: [],
+      goods:{},
+      shop:{}
     }
   },
   created() {
@@ -32,13 +47,36 @@ export default {
     //通过iid请求详情数据
     getDetail(this.iid).then((res) => {
       console.log(res);
-      this.topImg = res.result.itemInfo.topImages
+      const data = res.result
+      this.topImg = data.itemInfo.topImages
+
+      this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services),
+      //获取店铺信息
+      this.shop = new Shop(data.shopInfo)
     })
   },
- 
+  methods:{
+    loadMore() {
+     
+      this.$refs.scroll.scroll.finishPullUp()
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  #detail {
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+    height: 100vh;
+  }
+  .detail-nav {
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+  }
+  .content {
+    height: calc(100% - 44px);
+  }
 </style>
