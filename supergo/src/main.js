d30ec983f6ae7d622620095c94459683e22444ad
twotwowen <two_two_wen@163.com>
2021-09-13 14:34:58 +0800

    添加了fastclick与图片懒加载

--- supergo/src/main.js
+++ supergo/src/main.js
@@ -3,11 +3,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import FastClick from 'fastclick'
+import lazyPlugin from 'vue3-lazy'
+
 //import moment from 'moment'
-
 //将moment挂载到vue上
-
 //createApp(App).config.globalProperties.$moment = moment
 
-createApp(App).use(store).use(router).mount('#app')
+//解决移动端的300ms延迟
+FastClick.attach(document.body)
+    // createApp(App).mount('#app')
+
+createApp(App).use(store).use(router).use(lazyPlugin, { loading: require('assets/img/common/twowen.png') }).mount('#app')
     //Vue.config.productionTip = false
