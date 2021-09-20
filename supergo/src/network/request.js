b135d8f5fb58261ac2455c496e955b5767c08e15
twotwowen <89967213+twotwowen@users.noreply.github.com>
2021-09-20 19:00:07 +0800

    Update request.js

--- supergo/src/network/request.js
+++ supergo/src/network/request.js
@@ -4,7 +4,8 @@ export function request(config) {
     //创建axios的实例
     const instance = axios.create({
         //baseURL: 'http://123.207.32.32:8000',
-        baseURL: 'http://152.136.185.210:7878/api/m5',
+        //baseURL: '',
+      //接口已过期
         timeout: 10000
     })
 
